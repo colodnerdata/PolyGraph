e4777f530949822d5120bbe0119b25f924fea9c6
Colodner <colodner.data@gmail.com>
2026-03-09 12:54:40 -0400

    Create source/conf.py
    Add sphinx config

---
+++ docs/source/conf.py
@@ -0,0 +1,30 @@
+import os
+import sys
+
+sys.path.insert(0, os.path.abspath("../../src"))
+
+project = "PolyGraph"
+author = "Stephen Colodner"
+copyright = "2026, Stephen Colodner"
+release = "0.1.0"
+
+extensions = [
+    "sphinx.ext.autodoc",
+    "sphinx.ext.autosummary",
+    "sphinx.ext.napoleon",   # optional, harmless even with numpydoc
+    "numpydoc",
+]
+
+templates_path = ["_templates"]
+exclude_patterns = []
+
+html_theme = "furo"
+
+autosummary_generate = True
+autoclass_content = "class"
+add_module_names = False
+
+numpydoc_show_class_members = False
+numpydoc_class_members_toctree = False
+
+master_doc = "index"
