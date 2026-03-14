28e1a0e438e9141f7bbad01940a484e7ed0a3aab
Colodner <colodner.data@gmail.com>
2026-03-14 14:54:37 -0400

    Refactor cube and octahedron to prism-family generators

---
+++ src/polygraph/__init__.py
@@ -0,0 +1,21 @@
+"""Top-level PolyGraph public API."""
+
+from polygraph.generators import (
+    antiprism,
+    cube,
+    dodecahedron,
+    icosahedron,
+    octahedron,
+    prism,
+    tetrahedron,
+)
+
+__all__ = [
+    "tetrahedron",
+    "cube",
+    "octahedron",
+    "dodecahedron",
+    "icosahedron",
+    "prism",
+    "antiprism",
+]
---
+++ src/polygraph/generators/__init__.py
@@ -0,0 +1,20 @@
+"""Public generators API."""
+
+from polygraph.generators.platonic import (
+    cube,
+    dodecahedron,
+    icosahedron,
+    octahedron,
+    tetrahedron,
+)
+from polygraph.generators.prisms import antiprism, prism
+
+__all__ = [
+    "tetrahedron",
+    "cube",
+    "octahedron",
+    "dodecahedron",
+    "icosahedron",
+    "prism",
+    "antiprism",
+]
